88fb3fbe107f441eed4f0e283016caa6843e3eb0
Kaan Karagüney <yunusemreyilandil61@hotmail.com>
2024-02-08 23:09:30 +0300

    -Final project

---
+++ DgdFinal/ConsoleApp1/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiddleGame
+{
+    public class PlayerMovement
+    {
+        public Player Player { get; set; }
+        public Map Map { get; set; }
+
+        private Vector2 _coordinates;
+
+        public void MovePlayer(int x, int y)
+        {
+            int newXCoordinate = (int)_coordinates.X + x;
+            int newYCoordinate = (int)_coordinates.Y + y;
+
+            if (newXCoordinate >= Map.mapHorizontalBorder || newXCoordinate <= -Map.mapHorizontalBorder ||
+                newYCoordinate >= Map.mapVerticalBorder || newYCoordinate <= -Map.mapVerticalBorder)
+            {
+                Console.WriteLine("You can't go any further. Please move in the opposite direction.");
+                return;
+            }
+
+            _coordinates.X = newXCoordinate;
+            _coordinates.Y = newYCoordinate;
+
+            Console.WriteLine(_coordinates);
+            CheckForLocation(_coordinates);
+        }
+
+
+        public void CheckForLocation(Vector2 coordinates)
+        {
+            foreach (var point in Map._locations)
+            {
+                if (point.Coordinates == coordinates)
+                {
+                    point.OnReached(Player);
+                }
+            }
+        }
+    }
+}
